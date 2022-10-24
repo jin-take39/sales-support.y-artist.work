2132ae6972609c37cd756b9e9ea8615be8000183
take0029 <jin.takemitsu@gmail.com>
2022-10-24 09:55:33 +0900

    debugコード削除

--- public_html/laravel/exment/storage/app/plugins/DecisionEvent/Plugin.php
+++ public_html/laravel/exment/storage/app/plugins/DecisionEvent/Plugin.php
@@ -12,15 +12,11 @@ class Plugin extends PluginEventBase
     {
         $reDataList = array();
 
-        \Log::debug('Event called!');
-
         // 決定表のモデル取得
         $decision = CustomTable::getEloquent('decision_table')->getValueModel($this->custom_value->parent_id);
 
         // 計算
         if($this->custom_table->table_name == "deposit"){
-
-            \Log::debug($this->custom_table->table_name);
 
             // 決定表の親IDで見積・請求取得
             $dataList = CustomTable::getEloquent('deposit')->getValueModel()->query()->where('parent_id',$this->custom_value->parent_id)->get()->toArray();
@@ -69,7 +65,6 @@ class Plugin extends PluginEventBase
         ]);
         $decision->save();
 
-        \Log::debug(round(($decisionDataList['value']['total_amount']-$decisionDataList['value']['total_tax_exc_purchase']-$decisionDataList['value']['total_transport_exp']) / $decisionDataList['value']['total_amount'],10));
         return true;
     }
 
